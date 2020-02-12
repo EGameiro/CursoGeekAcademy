1f40374190ab330c57766215feb959edc5abbb5c
EGameiro <eduardo@digiplay.net.br>
2020-02-12 17:04:35 -0300

    Terceiro programa

---
+++ tipo_booleano.py
@@ -0,0 +1,14 @@
+#   Tipo Booleano vem da algebra booleana
+#   True ou False tem que ter necessariamente a primeira letra Maiuscula
+
+ativo = False
+
+print(ativo)
+
+#   Operações Basicas :
+
+#   Negação (not):
+#   Fazendo a negação, se o valor booleano for verdadeira o resultado é falso,
+#   se for falso o resultado será verdadeiro. Ou seja, sempre o contrario.
+
+print(not ativo)
