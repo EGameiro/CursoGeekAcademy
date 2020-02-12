8a1b739370289810f271827553885ab7b1d82148
EGameiro <eduardo@digiplay.net.br>
2020-02-12 16:48:20 -0300

    segundo Programa

---
+++ tipo_float.py
@@ -0,0 +1,21 @@
+#   Lembrar que o separador decimal é o PONTO e não a VIRGULA
+
+valor=1, 44
+print (valor)
+print(type(valor))
+
+valor = 1.44
+print(valor)
+print(type(valor))
+
+#   É possivel fazer dupla atribuição
+valor1, valor2 = 1, 44
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+
+# Podemos converter um Float para Int
+res=int(valor)
+print(res)
+print(type(res))
