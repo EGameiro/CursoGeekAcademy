d1471e15c66983fefbc8f040b3d6a60b69b3801a
EGameiro <eduardo@digiplay.net.br>
2020-02-18 16:22:54 -0300

    Quarto programa (exemplo do Palindromo)

--- tipo_string.py
+++ tipo_string.py
@@ -8,6 +8,7 @@ Em Python um dado é considerado String sempre que :
 4) Estiver entre aspas duplas triplas 
 """
 
+"""
 nome = 'Geek university'
 print (nome)
 print(type(nome))
@@ -27,3 +28,29 @@ print(type(nome))
 nome = 'Angelina \nJolie'
 print (nome)
 print(type(nome))
+"""
+nome='Geek University'
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.split()) #   Transforma em uma lista de Strings
+
+print (nome[0:4])   #   Slice de string
+
+print(nome[5:15])   #   Slice de string
+
+print(nome.split()[0])  #   pega a 1a ocorrencia da lista de string
+
+print(nome.split()[1])  #   pega a 2a ocorrencia da lista de string
+
+#   [::-1]  --> comece do primeiro elemento, vá até o último elemento e inverta
+print(nome[::-1])   #   Inversão da String
+
+print(nome.replace('G', 'P'))
+
+nome='ana'  #   ana é um palindromo
+print(nome)
+
+print(nome[::-1])
+
