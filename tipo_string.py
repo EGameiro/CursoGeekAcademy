036014379d24dd803758c5e57d2d5e70cb66bc5b
EGameiro <eduardo@digiplay.net.br>
2020-02-18 16:18:47 -0300

    Quarto programa

---
+++ tipo_string.py
@@ -0,0 +1,29 @@
+#   Tip String
+"""
+Em Python um dado é considerado String sempre que :
+
+1) Estiver entre aspas simples --> 'uma string'
+2) Estiver entre aspas duplas --> "uma string"
+3) Estiver entre aspas simples triplas --> '''uma string'''
+4) Estiver entre aspas duplas triplas 
+"""
+
+nome = 'Geek university'
+print (nome)
+print(type(nome))
+
+nome = "Geek university"
+print (nome)
+print(type(nome))
+
+nome = '''Geek university'''
+print (nome)
+print(type(nome))
+
+nome = "Gina's bar"
+print (nome)
+print(type(nome))
+
+nome = 'Angelina \nJolie'
+print (nome)
+print(type(nome))
