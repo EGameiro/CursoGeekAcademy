b1353b00d33562fb3e78a4d92a529633c93977ab
EGameiro <eduardo@digiplay.net.br>
2020-02-18 17:13:06 -0300

    Quinto Programa

---
+++ escopo_de_variavel.py
@@ -0,0 +1,21 @@
+"""
+Escopo de variaveis
+
+1 - Variáveis globais;
+    Variaveis globais são reconhecidas em todo o programa
+
+2 - Variaveis locais;
+    Variaveis locais são reconhecidas apenasno bloco onde foram declaradas, ou seja seu escopo
+    está limitado ao bloco onde foi declarada.
+
+Python é uma linguagem de tipagem dinâmica. Isso significa que
+ao declararmos uma variavel, nós não colocamos o tipo do dado.
+
+"""
+numero =42
+print(numero)
+print(type(numero))
+
+numero = 'Geek'
+print(numero)
+print(type(numero))
