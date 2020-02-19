2e63a33050e3b35a1730c5b6a46cf952ab1e1d6c
EGameiro <eduardo@digiplay.net.br>
2020-02-19 16:52:47 -0300

    Estrutura Condicional

---
+++ condicionais_if_else_elif.py
@@ -0,0 +1,16 @@
+"""
+Estrutura condicionais
+IF, ELSE, ELIF
+"""
+
+idade=28
+
+if idade < 18:
+   print("Menor de Idade")
+elif idade == 18:
+    print("Tem 18 Anos")
+elif idade == 28:
+    print("Tem 28 Anos")
+else:
+   print("Maior de idade")
+  
