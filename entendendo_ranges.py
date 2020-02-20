c64c42849ba064f7cecea6be15f5c08d3d9f1e8d
EGameiro <eduardo@digiplay.net.br>
2020-02-20 16:30:49 -0300

    Ranges

---
+++ entendendo_ranges.py
@@ -0,0 +1,25 @@
+"""
+Ranges
+
+- Precisamos conhecer o Loop par ausar os ranges
+- Precisamos conehcer os Ranges para trabalhar melhor com Loops
+"""
+
+for num in range(11):
+    print(num)
+
+print('----------------------')
+
+for num in range(1,11):
+    print(num)
+
+print('----------------------')
+
+for num in range(5,11):
+    print(num)
+
+print('----------------------')
+#range(valor_deInicio, valor_final, passo)
+
+for num in range(1,10,2):
+    print(num)
